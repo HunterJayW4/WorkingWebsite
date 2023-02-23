9db683ee5cbca82ea2a0be2d1361c4a75b8beed0
HunterJayW4 <hunterjayw@gmail.com>
2023-02-22 19:06:12 -0700

    updated about page

--- about.php
+++ about.php
@@ -3,6 +3,15 @@
 
 <html>
     <link rel="stylesheet" href="Style.css">
-
+        <body>
+            <p>
+                This is the about page for Idaho River Shorts, which is still under construction. There will be a somewhat
+                extensive description of what Idaho River Shorts was founded on, and how we operate as a company. We are
+                so very excited to offer our loyal customers the best shorts known to mankind. We use a variety of fabrics
+                in our garments, including cotton, silk, denim, lace, leather, linen, satin, crepe, cashmere, chiffon, canvas,
+                damask, georgette, gingham, jersey, merino, muslin, organza, polyester, spandex, suede, tafetta, toile, tweed,
+                twill, and viscose.
+            </p>
+        </body>
     <title>Idaho River Shorts - Shop</title>
 </html>

e9db4f2eecd75af941e65d444a954db530fbc335
HunterJayW4 <hunterjayw@gmail.com>
2023-02-22 18:33:22 -0700

    updated Header

--- Header.php
+++ Header.php
@@ -9,7 +9,7 @@
         <hr>
         <div id="Nav">
             <ul class="nav">
-                <li><a href="home.php">HOME</a></li>
+                <li><a href="index.php">HOME</a></li>
                 <li><a href="about.php">ABOUT</a></li>
                 <li><a href="shop.php">SHOP</a></li>
                 <li><a href="contact.php">CONTACT</a></li>
